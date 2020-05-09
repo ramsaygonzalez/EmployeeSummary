7d23a32a67c507950b5c0ff8c45f200def90412a
ramsayg <ramsaygonzalez@outlook.com>
2020-05-08 22:42:11 -0400

    upddated app.js file with code to render html file with information gathered from the user.

--- app.js
+++ app.js
@@ -44,7 +44,8 @@ function getTeamInfo() {
     ).then(function (data) {
         //assigns role and adds user inputs to team member array
         data["role"] = "Manager";
-        teamMembers.push(data);
+        const manager = new Manager(data.name, data.id, data.email, data.officeNumber)
+        teamMembers.push(manager);
 
     }).then(function () {
         //ask the user if he/she would like to add additional inputs.
@@ -69,14 +70,16 @@ function getTeamInfo() {
 
             }
             else {
+                exportTeam()
                 return console.log(teamMembers)
-                render(teamMembers)
+
             }
         })
     })
 }
 
 function getEngineerInfo() {
+    //Prompts the user for Engineer Info
     inquirer.prompt([
         {
             type: "input",
@@ -101,8 +104,10 @@ function getEngineerInfo() {
 
     ]
     ).then(function (data) {
+        //assigns role and adds user inputs to team member array
         data["role"] = "Engineer";
-        teamMembers.push(data);
+        const engineer = new Engineer(data.name, data.id, data.email, data.github)
+        teamMembers.push(engineer);
     }).then(function () {
         inquirer.prompt([
             {
@@ -125,8 +130,9 @@ function getEngineerInfo() {
 
             }
             else {
+                exportTeam()
                 return console.log(teamMembers)
-                render(teamMembers)
+
 
             }
         })
@@ -134,6 +140,7 @@ function getEngineerInfo() {
 };
 
 function getInternInfo() {
+    //prompts user for intern details
     inquirer.prompt([
         {
             type: "input",
@@ -152,14 +159,16 @@ function getInternInfo() {
         },
         {
             type: "input",
-            name: "github",
+            name: "school",
             message: "What is your intern's school?"
         },
 
     ]
     ).then(function (data) {
+        //assigns role and adds user inputs to team member array
         data["role"] = "Intern";
-        teamMembers.push(data);
+        const intern = new Intern(data.name, data.id, data.email, data.school)
+        teamMembers.push(intern);
     }).then(function () {
         inquirer.prompt([
             {
@@ -182,33 +191,14 @@ function getInternInfo() {
 
             }
             else {
+                exportTeam()
                 return console.log(teamMembers)
-                render(teamMembers)
+
             }
         })
     })
 };
-
-
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
+//renders html file with team info
+function exportTeam() {
+    fs.writeFileSync(outputPath, render(teamMembers));
+}
